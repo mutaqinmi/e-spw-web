4f93cec9f538361f5677df52893f7963b63f1761
mutaqinmi <mutaqinmi2586@gmail.com>
2024-06-06 08:20:01 +0700

    feat: adjusting with html version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,57 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import './global.module.css';
+import './style.module.css';
+import Link from 'next/link';
+import Image from 'next/image';
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+export default function Home(){
+    return <>
+        <Navbar initialIndex='1'/>
+        <Header/>
+    </>
+}
+
+function Navbar(prop: any){
+    console.log(prop.initialIndex);
+    return <nav className='navbar'>
+        <div className='brand'>
+            <Link href={'/'}>
+                <Image src={'/icons/espw-white.png'} alt='eSPW' className='brand-logo'></Image>
+            </Link>
         </div>
-      </div>
+        <div className='menu'>
+            <div className='menu-button'>
+                <div className='garis1'></div>
+                <div className='garis2'></div>
+                <div className='garis3'></div>
+            </div>
+            <div className='menu-link hide'>
+                <Link href={'/'} className='nav-link active'>Beranda</Link>
+                <Link href={'/'} className='nav-link'>Tentang</Link>
+                <Link href={'/'} className='nav-link'>Fitur</Link>
+                <Link href={'/'} className='nav-link'>FAQ</Link>
+                <Link href={'/login-guru'} className='nav-link'>Guru PKK</Link>
+            </div>
+            <div className='download'>
+                <Link href={'/app/espw-v0.1.0-alpha.apk'} download={'espw-app-v0.1.0-alpha'} className='download-button'>Download</Link>
+            </div>
+        </div>
+    </nav>
+}
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+function Header(){
+    return <div className='header'>
+        <div className='circle crc1'></div>
+        <div className='circle crc2'></div>
+        <div className='circle crc3'></div>
+        <div className='header-title'>
+            <h1>Beli apa aja<br/>bisa dimana aja</h1>
+            <p>Dapatkan produk yang kamu inginkan dengan kemudahan berbelanja online hanya di eSPW!</p>
+            <div className='download'>
+                <Link href={'/app/espw-v0.1.0-alpha.apk'} download={'espw-app-v0.1.0-alpha'} className='download-button header-download-button'>Download</Link>
+            </div>
+            <div className='header-image'>
+                <Image src={'/images/frame.png'} alt='eSPW Mobile'></Image>
+            </div>
+        </div>
+    </div>
 }
